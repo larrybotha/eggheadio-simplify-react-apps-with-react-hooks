1cd6a90a200601a2a672f283d5b72bdd6597b38d
Larry Botha <larry@fixate.it>
2019-06-30 21:55:23 -0500

    add annotations for class component before being converted to a component function

--- src/github-client.original.js
+++ src/github-client.original.js
@@ -30,6 +30,13 @@ async function authWithGitHub() {
 const history = createHistory(window)
 
 class GitHubClientProvider extends React.Component {
+  /*
+   * state in here can be initialised using useState
+   *
+   * Because one of the values in state is derived from props, and only once
+   * when the constructor is called on mount, we can use the initialiser
+   * function that useState accepts to set the initial state
+   */
   constructor(...args) {
     super(...args)
     this.state = {error: null}
@@ -42,6 +49,19 @@ class GitHubClientProvider extends React.Component {
       }
     }
   }
+
+  /*
+   * This componentDidMount is definined side effects - we can place them inside
+   * useEffect instead
+   *
+   * this.unsubscribeHistory is defined here purely so that it may be called in
+   * componentWillUnmount
+   *
+   * if a function is returned in useEffect, that functino will be called every
+   * time a value in the dependency array changes, or when the component
+   * unmounts. Because we want the unsubscribe to only occur when the component
+   * unmounts, we can pass an empty dependency array to useEffect
+   */
   componentDidMount() {
     if (!this.state.client) {
       navigate('/')
@@ -52,9 +72,11 @@ class GitHubClientProvider extends React.Component {
       }
     })
   }
+
   componentWillUnmount() {
     this.unsubscribeHistory()
   }
+
   getClient = token => {
     const headers = {Authorization: `bearer ${token}`}
     const client = new GraphQLClient('https://api.github.com/graphql', {
@@ -65,11 +87,17 @@ class GitHubClientProvider extends React.Component {
       logout: this.logout,
     })
   }
+
+  /*
+   * this function uses this.setState. In the hooks implementation it'll use the
+   * state update function returned from useState
+   */
   logout = () => {
     window.localStorage.removeItem('github-token')
     this.setState({client: null, error: null})
     navigate('/')
   }
+
   login = async () => {
     const data = await authWithGitHub().catch(error => {
       console.log('Oh no', error)
@@ -78,6 +106,7 @@ class GitHubClientProvider extends React.Component {
     window.localStorage.setItem('github-token', data.token)
     this.setState({client: this.getClient(data.token)})
   }
+
   render() {
     const {client, error} = this.state
     const {children} = this.props
